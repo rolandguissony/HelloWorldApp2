06389e19d4c434fe92697c4ad796daa9c9e8153a
Roland Ulrich Guissony <hello@rolandguissony.com>
2018-06-15 01:35:06 +0200

    Création du programme HelloWorldApp2 par Roland Ulrich Guissony... ♥♥♥

---
+++ src/main/java/rolandulrichguissony/HelloWorldApp2/App.java
@@ -0,0 +1,22 @@
+package rolandulrichguissony.HelloWorldApp2;
+/************************************************************
+ * The HelloWorldApp class implements an application that   *
+ * simply prints "Hello World!" to standard outpout.        *
+ ************************************************************/
+
+/************************************************************
+ * ©2018 Roland Ulrich Guissony, All rights reserved        *
+ * Java/Java EE developer                                   *
+ * Paris Saclay University's Entrepreneur student           *
+ * Electronic and Informatic Engineer                       *
+ ************************************************************/
+public class App 
+{
+	/***************
+	 * @param args *
+	 ***************/
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! I am Roland Ulrich Guissony. I and love programming..." );
+    }
+}
