520fd02ab56ce7d3f6d152f84d0e1e2c6f023826
Roland Ulrich Guissony <hello@rolandguissony.com>
2018-06-16 20:31:47 +0200

    Rien de spécial n'a été fait. Juste un coup d'oeil sur les commentaires...

--- src/test/java/rolandulrichguissony/HelloWorldApp2/AppTest.java
+++ src/test/java/rolandulrichguissony/HelloWorldApp2/AppTest.java
@@ -4,14 +4,14 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+/*****************************
+ * Unit test for simple App. *
+ *****************************/
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    /*********************
+     * Rigorous Test :-) *
+     *********************/
     @Test
     public void shouldAnswerWithTrue()
     {
